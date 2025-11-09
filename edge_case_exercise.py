a194799353cf4dc8985f9920592285c7e32161ab
anaszbedat1-wq <anaszbedat1@gmail.com>
2025-11-09 22:48:35 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,15 +1,46 @@
+
 def move(my_list, direction):
-
-    # Finds the index of the one in the list
     index_of_one = my_list.index(1)
-
-    # Move the one to the left or to the right
+    
     if direction == 'right':
+        if index_of_one == len(my_list) - 1:
+            return my_list
         my_list[index_of_one] = 0
         my_list[index_of_one + 1] = 1
 
     elif direction == 'left':
+        if index_of_one == 0:
+            return my_list
         my_list[index_of_one] = 0
         my_list[index_of_one - 1] = 1
 
-    return my_list
+    return my_list 
+    
+    def move(the_pig_location, direction):
+    
+    index_of_one = the_pig_location.index(1)
+
+    
+    if direction == "left":
+        if index_of_one > 0:  
+            the_pig_location[index_of_one] = 0
+            the_pig_location[index_of_one - 1] = 1
+
+ 
+    elif direction == "right":
+        if index_of_one < len(the_pig_location) - 1: 
+            the_pig_location[index_of_one] = 0
+            the_pig_location[index_of_one + 1] = 1
+
+    
+    return the_pig_locatio
+    def move(lst, direction):
+    index_of_one = lst.index(1)
+
+    if direction == "right" and index_of_one < len(lst) - 1:
+        lst[index_of_one], lst[index_of_one + 1] = lst[index_of_one + 1], lst[index_of_one]
+
+    elif direction == "left" and index_of_one > 0:
+        lst[index_of_one], lst[index_of_one - 1] = lst[index_of_one - 1], lst[index_of_one]
+
+    return lst
